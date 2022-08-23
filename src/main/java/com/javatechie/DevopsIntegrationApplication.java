0f5bbfa35a1de840a158c54cb15f89b17edb9c3d
BabyMounicaAndey <babymounicaandey@gmail.com>
2022-08-23 16:00:34 +0530

    first commit

---
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -0,0 +1,14 @@
+package com.javatechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsIntegrationApplication.class, args);
+		System.out.println("Welcome to devops integration...");
+	}
+
+}
